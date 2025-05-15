d38bc1b18296c4062598dcf105e0ac25937687e5
KomfortKimko <komfortkimk@gmail.com>
2025-05-15 15:52:42 +0300

    changes to the api endpoint

--- client/src/api/ai.js
+++ client/src/api/ai.js
@@ -1,5 +1,5 @@
 export async function getRecipeFromBackend(ingredients) {
-    const res = await fetch("https://chef-claude-c7z4.onrender.com", {
+    const res = await fetch("https://chef-claude-c7z4.onrender.com/api/recipe", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ ingredients }),
